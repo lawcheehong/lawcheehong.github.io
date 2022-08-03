cf755705580c0c7dc03ceda02b5ead880d3b5fd1
lawcheehong <lawcheehong0511@e.newera.edu.my>
2022-08-03 18:52:49 +0800

    Update

--- groupassignment/js/scripts.js
+++ groupassignment/js/scripts.js
@@ -1,26 +1,36 @@
 window.addEventListener("scroll", function () {
-    var t = document.querySelector("#back-top");
-    t.classList.toggle("sticky", window.scrollY > 100);
-})
+  var t = document.querySelector("#back-top");
+  t.classList.toggle("sticky", window.scrollY > 100);
+});
 
-document.addEventListener("DOMContentLoaded", function(){
-    el_autohide = document.querySelector('.autohide');
+// https://bootstrap-menu.com/detail-autohide.html
+document.addEventListener("DOMContentLoaded", function () {
+  el_autohide = document.querySelector(".autohide");
 
-    navbar_height = document.querySelector('.navbar').offsetHeight;
-     
-    if(el_autohide){
-      var last_scroll_top = 0;
-      window.addEventListener('scroll', function() {
-            let scroll_top = window.scrollY;
-           if(scroll_top < last_scroll_top) {
-                el_autohide.classList.remove('scrolled-down');
-                el_autohide.classList.add('scrolled-up');
-            }
-            else if(scroll_top > 100){
-                el_autohide.classList.remove('scrolled-up');
-                el_autohide.classList.add('scrolled-down');
-            }
-            last_scroll_top = scroll_top;
-      }); 
-    }
-}); 
+  if (el_autohide) {
+    var last_scroll_top = 0;
+    window.addEventListener("scroll", function () {
+      let scroll_top = window.scrollY;
+      let menu_collapse = document
+        .querySelector("[aria-expanded]")
+        ?.getAttribute("aria-expanded");
+      if (scroll_top < last_scroll_top) {
+        el_autohide.classList.remove("scrolled-down");
+        el_autohide.classList.add("scrolled-up");
+      } else if (scroll_top > 100) {
+        if (menu_collapse == "false") {
+          el_autohide.classList.remove("scrolled-up");
+          el_autohide.classList.add("scrolled-down");
+        }
+      }
+      last_scroll_top = scroll_top;
+    });
+  }
+
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
